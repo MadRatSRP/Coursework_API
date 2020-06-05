f6f87c0ddd4fa191412eca4fe064de61a545dea1
MadRat <mischa.alpeew@yandex.ru>
2020-06-05 19:01:11 +0300

    SearchFoodVideos: added image loading and clicklistener for youtube video

--- app/src/main/java/com/madrat/kursovaya/adapters/SearchFoodVideosAdapter.kt
+++ app/src/main/java/com/madrat/kursovaya/adapters/SearchFoodVideosAdapter.kt
@@ -1,11 +1,14 @@
 package com.madrat.kursovaya.adapters
 
+import android.content.Intent
+import android.net.Uri
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.madrat.kursovaya.R
 import com.madrat.kursovaya.model.search_food_videos.Video
 import com.madrat.kursovaya.util.inflate
+import com.madrat.kursovaya.util.loadImageFromUrl
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.list_search_food_videos.*
 
@@ -31,7 +34,18 @@ class SearchFoodVideosAdapter
     inner class SearchFoodVideosHolder internal constructor(override val containerView: View)
         : RecyclerView.ViewHolder(containerView), LayoutContainer {
         fun bind(video: Video) {
-            textView.text = video.youTubeId
+            title.text = video.title
+
+            video_preview_image.loadImageFromUrl(video.thumbnail)
+
+            watch_video_on_youtube_button.setOnClickListener {
+                val videoUrl = containerView.context.getString(
+                    R.string.base_url_youtube, video.youTubeId
+                )
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(videoUrl)
+                containerView.context.startActivity(intent)
+            }
         }
     }
 }
