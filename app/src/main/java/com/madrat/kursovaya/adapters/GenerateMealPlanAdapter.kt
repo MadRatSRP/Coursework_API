d414a6f98252cda3157e2a804797f73254e01baa
MadRat96 <mischa.alpeew@yandex.ru>
2021-07-24 14:25:35 +0300

    Updated buildToolsVersion to 31.0.0, downgraded to 30.0.3 (latest version is corrupted) Updated ViewBinding initialization in build.gradle Disabled kotlin-android-extensions plugin, refactored its usage in GenerateMealPlanAdapter

--- app/src/main/java/com/madrat/kursovaya/adapters/GenerateMealPlanAdapter.kt
+++ app/src/main/java/com/madrat/kursovaya/adapters/GenerateMealPlanAdapter.kt
@@ -2,20 +2,17 @@ package com.madrat.kursovaya.adapters
 
 import android.content.Intent
 import android.net.Uri
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.madrat.kursovaya.R
+import com.madrat.kursovaya.databinding.ListGenerateMealPlanBinding
 import com.madrat.kursovaya.fragments.GenerateMealPlanViewDirections
 import com.madrat.kursovaya.model.generate_meal_plan.Meal
-import com.madrat.kursovaya.util.inflate
 import com.madrat.kursovaya.util.loadImageFromUrl
-import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.list_generate_meal_plan.*
 
-class GenerateMealPlanAdapter
-    : RecyclerView.Adapter<GenerateMealPlanAdapter.GenerateMealPlanHolder>() {
+class GenerateMealPlanAdapter : RecyclerView.Adapter<GenerateMealPlanAdapter.GenerateMealPlanHolder>() {
     private val listOfMeals = ArrayList<Meal>()
 
     fun updateListOfMeals(newListOfMeals: ArrayList<Meal>) {
@@ -24,61 +21,71 @@ class GenerateMealPlanAdapter
         this.notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenerateMealPlanHolder
-            = GenerateMealPlanHolder(parent.inflate(R.layout.list_generate_meal_plan))
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenerateMealPlanHolder {
+        val binding = ListGenerateMealPlanBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return GenerateMealPlanHolder(binding)
+    }
+    
     override fun onBindViewHolder(holder: GenerateMealPlanHolder, position: Int)
-            = holder.bind(listOfMeals[position])
+        = holder.bind(listOfMeals[position])
 
     override fun getItemCount(): Int
-            = listOfMeals.size
+        = listOfMeals.size
 
-    inner class GenerateMealPlanHolder internal constructor(override val containerView: View)
-        : RecyclerView.ViewHolder(containerView), LayoutContainer {
+    inner class GenerateMealPlanHolder (private val binding: ListGenerateMealPlanBinding)
+        : RecyclerView.ViewHolder(binding.root) {
         fun bind(meal: Meal) {
-            title.text = meal.title
-
-            menu_image.loadImageFromUrl(
-                containerView.context.getString(
-                    R.string.base_url_recipes, meal.id, meal.imageType
+            with(binding) {
+                title.text = meal.title
+                
+                val holderContext = binding.root.context
+                
+                menuImage.loadImageFromUrl(
+                    holderContext.getString(
+                        R.string.base_url_recipes, meal.id, meal.imageType
+                    )
                 )
-            )
-
-            ready_in_minutes_value.text = meal.readyInMinutes.toString()
-            servings_value.text = meal.servings.toString()
-
-            show_recipe_website_button.setOnClickListener {
-                val intent = Intent(Intent.ACTION_VIEW)
-                intent.data = Uri.parse(meal.sourceUrl)
-                containerView.context.startActivity(intent)
-            }
-
-            showRecipeEquipmentButton.setOnClickListener {
-                val action = GenerateMealPlanViewDirections.actionGenerateMealPlanToGetRecipeEquipmentById(
-                    meal.id
-                )
-                Navigation.findNavController(containerView).navigate(action)
-            }
-
-            showRecipeIngredientsButton.setOnClickListener {
-                val action = GenerateMealPlanViewDirections.actionGenerateMealPlanToGetRecipeIngredientsByIdView(
-                    meal.id
-                )
-                Navigation.findNavController(containerView).navigate(action)
-            }
-
-            showRecipeNutritionValueButton.setOnClickListener {
-                val action = GenerateMealPlanViewDirections.actionGenerateMealPlanToGetRecipeNutritionWidgetByIdView(
-                    meal.id
-                )
-                Navigation.findNavController(containerView).navigate(action)
-            }
-
-            show_similair_recipes_button.setOnClickListener {
-                val action = GenerateMealPlanViewDirections.actionGenerateMealPlanToGetSimilarRecipesView(
-                    meal.id
-                )
-                Navigation.findNavController(containerView).navigate(action)
+    
+                readyInMinutesValue.text = meal.readyInMinutes.toString()
+                servingsValue.text = meal.servings.toString()
+    
+                showRecipeWebsiteButton.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse(meal.sourceUrl)
+                    holderContext.startActivity(intent)
+                }
+    
+                showRecipeEquipmentButton.setOnClickListener {
+                    val action = GenerateMealPlanViewDirections.actionGenerateMealPlanToGetRecipeEquipmentById(
+                        meal.id
+                    )
+                    Navigation.findNavController(root).navigate(action)
+                }
+    
+                showRecipeIngredientsButton.setOnClickListener {
+                    val action = GenerateMealPlanViewDirections.actionGenerateMealPlanToGetRecipeIngredientsByIdView(
+                        meal.id
+                    )
+                    Navigation.findNavController(root).navigate(action)
+                }
+    
+                showRecipeNutritionValueButton.setOnClickListener {
+                    val action = GenerateMealPlanViewDirections.actionGenerateMealPlanToGetRecipeNutritionWidgetByIdView(
+                        meal.id
+                    )
+                    Navigation.findNavController(root).navigate(action)
+                }
+    
+                showSimilairRecipesButton.setOnClickListener {
+                    val action = GenerateMealPlanViewDirections.actionGenerateMealPlanToGetSimilarRecipesView(
+                        meal.id
+                    )
+                    Navigation.findNavController(root).navigate(action)
+                }
             }
         }
     }
